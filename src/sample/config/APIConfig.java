ba782c14603a25075b4b2b8ff4f44306dedfded2
glebkalinin <kaliningleb25@gmail.com>
2018-05-29 13:07:20 +0300

    master: added APIConfig

---
+++ src/sample/config/APIConfig.java
@@ -0,0 +1,5 @@
+package sample.config;
+
+public final class APIConfig {
+    public static final String API_KEY = "trnsl.1.1.20180522T073638Z.badb210435a02d79.57e7d7c9662a55fae77e89a8e10acaae0a9c2992";
+}
